ff05b2ff2548e4f81b416dce0a71fa3af46d2e43
Ezall2001 <armenre2001@outlook.fr>
2023-02-10 22:51:45 +0100

    updated Log.console to accept all objects instead of string only

--- pidev_java/src/config/Log.java
+++ pidev_java/src/config/Log.java
@@ -6,11 +6,26 @@ public class Log {
 
   }
 
-  public static void console(String message) {
+  private static String verifier_implementation_toString(Object obj) {
+    try {
+      if (obj.getClass().getMethod("toString").getDeclaringClass() != Object.class)
+        return obj.toString();
+      return String.format("%s n'implemente pas la methode toString", obj.getClass());
+
+    } catch (NoSuchMethodException e) {
+      return e.getMessage();
+    }
+
+  }
+
+  public static void console(Object obj) {
+    String message = verifier_implementation_toString(obj);
     System.out.println(message);
   }
 
-  public static void fichier(String message) {
+  /// TODO: finish the file output log
+  public static void fichier(Object obj) {
+    String message = verifier_implementation_toString(obj);
     System.out.println(message);
   }
 
