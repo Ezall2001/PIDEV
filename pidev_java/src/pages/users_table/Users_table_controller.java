8f843d389ccde4c9c607a2020c11b638afcc0a4f
eyaharbi <80215892+eyaharbi@users.noreply.github.com>
2023-03-07 14:03:19 +0100

    eya users table not fully completed

--- pidev_java/src/pages/users_table/Users_table_controller.java
+++ pidev_java/src/pages/users_table/Users_table_controller.java
@@ -1,12 +1,21 @@
 package pages.users_table;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import dialogs.profile_input.Profile_input_controller;
+import entities.User;
+import entities.User.Type;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import services.User_service;
+import utils.Router;
+import utils.Table_view_helpers;
 
 public class Users_table_controller implements Initializable {
 
@@ -17,7 +26,55 @@ public class Users_table_controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        TableView<User> table = new TableView<>();
+
+        TableColumn<User, String> first_name_column = new TableColumn<>("Prénom");
+        first_name_column.setCellValueFactory(new PropertyValueFactory<User, String>("_first_name"));
+
+        TableColumn<User, String> last_name_column = new TableColumn<>("Nom");
+        last_name_column.setCellValueFactory(new PropertyValueFactory<User, String>("_last_name"));
+
+        TableColumn<User, String> email_column = new TableColumn<>("Email");
+        email_column.setCellValueFactory(new PropertyValueFactory<User, String>("_email"));
+
+        TableColumn<User, Number> age_column = new TableColumn<>("Age");
+        age_column.setCellValueFactory(new PropertyValueFactory<User, Number>("_age"));
+
+        TableColumn<User, String> bio_column = new TableColumn<>("Biographie");
+        bio_column.setCellValueFactory(new PropertyValueFactory<User, String>("_bio"));
+        bio_column = Table_view_helpers.set_text_wrap_cell(bio_column);
+        bio_column.setMinWidth(500);
+
+        TableColumn<User, Number> score_column = new TableColumn<>("Niveau");
+        age_column.setCellValueFactory(new PropertyValueFactory<User, Number>("_age"));
+
+        table = Table_view_helpers.add_action_column(table,
+                (User user) -> {
+                    Router.render_dialog("Profile_input",
+                            (Profile_input_controller controller) -> controller.hydrate(user));
+                },
+                (User user) -> {
+                    if (!Table_view_helpers.delete_confirmation())
+                        return;
+
+                    user_service.delete(user);
+                    Router.render_dialog("Users_table", null);
+                });
+
+        List<User> users = user_service.get_all();
+        // for (User user : users) {
+        // if (user.get_type() == Type.STUDENT) {
+
+        //     table.getItems().add(user);
+
+        // }
+        table.getItems().addAll(users);
+
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        table = Table_view_helpers.set_style(table);
+
+        table_wrapper.getChildren().add(table);
 
     }
-
+    // }
 }
