9e693a6ce2f7f022c592f4cd4920f5dd3451bedc
Najiba <najwagr12@gmail.com>
2023-03-07 14:12:51 +0100

    subject_table

---
+++ pidev_java/src/dialogs/subject_input/Subject_input_controller.java
@@ -0,0 +1,15 @@
+package dialogs.subject_input;
+
+import dialogs.Base_dialog_controller;
+import entities.Subject;
+
+public class Subject_input_controller extends Base_dialog_controller {
+    public void hydrate(Subject subject) {
+
+    }
+
+    public void hydrate() {
+
+    }
+
+}
