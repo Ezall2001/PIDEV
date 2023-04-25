89e5488847a8ef1ae82ec9272b8ef04e2d1b48e0
Nour <nourelhouda.kchaou@esprit.tn>
2023-04-25 03:43:01 +0200

    added qr code

--- pidev_web/src/Controller/TestResultController.php
+++ pidev_web/src/Controller/TestResultController.php
@@ -6,59 +6,82 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
 use App\Repository\TestsRepository;
 use App\Repository\TestResultsRepository;
+use App\Repository\TestQsRepository;
 use App\Repository\UsersRepository;
+use App\Entity\Users;
+use App\Entity\TestResults;
+use Endroid\QrCode\Builder\BuilderInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Endroid\QrCode\Label\Margin\Margin;
+use DateTime;
 
-use Dompdf\Dompdf;
+
+
+use Dompdf\Dompdf as Dompdf;
 use Dompdf\Options;
 
 class TestResultController extends AbstractController
 {
-    #[Route('/test/result', name: 'app_test_result')]
-    public function index(): Response
+  
+    #[Route('/check/{id}', name: 'check', methods: "POST")]
+    public function testScore(UsersRepository $userRep,TestResultsRepository $resRep, TestQsRepository $qsRepo,Request $request,$id,TestsRepository $testRep, ManagerRegistry $mr): Response
     {
-        return $this->render('test_result/index.html.twig', [
-            'controller_name' => 'TestResultController',
-        ]);
+        $jsonContent = $request->getContent();
+        $data = json_decode($jsonContent, true);
+        $responses = $data['responses'];
+
+            $nbCorrectOption = sizeof($responses);
+            for($i=0 ; $i<sizeof($responses) ;$i++){
+                $testQuestion = $qsRepo->find($responses[$i]["id"]);
+                
+                    if($responses[$i]["value"]!=$testQuestion->getCorrectOption()){
+                        $nbCorrectOption--;
+                    }
+            }
+
+            $em=$mr->getManager();
+            $result = $resRep->findOneBy(
+                ['tests' => $id,
+                 'users' => 530,
+                ]
+            );
+
+            if($result==null){
+
+                $result = new TestResults();
+                $user = $userRep->find(530);
+                $test = $testRep->find($id);
+
+                $result->setUser($user);
+                $result->setTest($test);
+                $result->setMark($nbCorrectOption);
+
+                $em->persist($result);
+                $em->flush();
+            }
+
+            else {
+                 $result->setMark($nbCorrectOption);
+                 $em->flush();
+            }
+      
+     return $this->json(['nbCorrectOption'=>$nbCorrectOption],200);
+
     }
 
+    #[Route('/result/{id}', name: 'resultDownload')]
+    public function resultDataDowload(TestResultsRepository $rep,$id,BuilderInterface $qrBuilder): Response
     
-    // #[Route('/insertScore', name: 'insertScore')]
-    // public function insertScore($score): Response
-    // {
-    //     $entityManager = $this->getDoctrine()->getManager();
-
-    //     $user=$userRep->find(530);
-    //     $test=$testRep->find(476);
-
-    //     $testRep=$entityManager->getRepository(Tests::class);
-    //     $userRep=$entityManager->getRepository(Users::class);
-    //     $resultRep=$entityManager->getRepository(Tests::class);
-
-    //     insertScore($score,$user,$test);
-        
-    //     return $this->render('dummy.html.twig');
-    // }
-
-    #[Route('/result', name: 'result')]
-    public function resultData(TestResultsRepository $rep): array
     {
-        $userid = 530;
-        $testId = 476;
-        $result = $rep->getUserResult($userid,$testId);
-        return $result;
-    }
-
-    #[Route('/result/download', name: 'resultDownload')]
-    public function resultDataDowload(TestResultsRepository $rep): Response
-    {
-        $result =$this->resultData($rep);
-
         // police default
+
         $pdfOptions = new Options();
         $pdfOptions->set('defaultFont','Arial');
         $pdfOptions->setIsRemoteEnabled(true);
+        //$pdfOptions->set('chroot', [__DIR__.'/public', __DIR__.'/images']);
 
         //
 
@@ -71,12 +94,27 @@ class TestResultController extends AbstractController
             ]
             ]);
 
+            $qrResult = $qrBuilder
+            ->size(400)
+            ->margin(20)
+            ->data('https://www.facebook.com/profile.php?id=100090591443416&mibextid=ZbWKwL')
+            ->build();
+    
+            $qrImage = $qrResult->getDataUri();
+
         $domPdf->setHTTPContext($context);
+        $domPdf->set_option('isRemoteEnabled',TRUE);
 
-        //  generating html
+       
 
+        $date = new DateTime();
+        $formattedDate = $date->format('d/m/yy');
+
+    //  generating html
         $html = $this->renderView('passage_test/result.html.twig',[
-            'result' => $result,
+            'result' => $rep->getUserResult(530,$id), // ! current user 
+            'qrImage' => $qrImage,
+            'date' => $formattedDate, 
         ]);
 
         $domPdf->loadHtml($html);
@@ -96,5 +134,25 @@ class TestResultController extends AbstractController
         return new Response();
     }
 
+    #[Route('/getResults', name: 'getResults')]
+    public function getAll(TestResultsRepository $rep): Response
+    {
+        return $this->render('test_result/results_list.html.twig', [
+            'result' => $rep->findAll(),
+        ]);
+    }
+
+
+    #[Route('/searchUser', name: 'searchUser', methods: "GET")]
+    public function searchUser(UsersRepository $userRep,NormalizerInterface $normalizer, Request $req): Response
+    {
+        
+        $requestString=$req->get('users');
+        $users = $userRep->findUserByName($requestString);
+        $jsonContent = $normalizer->normalize($users,'json',['groups'=>'users']);
+        $retour = json_encode($jsonContent);
+
+        return new Response($retour);
+    }
 
 }
